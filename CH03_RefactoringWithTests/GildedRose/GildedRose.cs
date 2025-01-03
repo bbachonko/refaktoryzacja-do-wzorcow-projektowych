aba9f3c4d2e5a3f1a5a880dd1745515d1f9ab43f
Barlomiej <b.bartek37@gmail.com>
2025-01-03 21:05:38 +0100

    zaj 20250103 gildedrose

--- CH03_RefactoringWithTests/GildedRose/GildedRose.cs
+++ CH03_RefactoringWithTests/GildedRose/GildedRose.cs
@@ -1,89 +1,86 @@
 ﻿using System.Collections.Generic;
 
-namespace GildedRoseKata;
-
-public class GildedRose
+namespace GildedRoseKata
 {
-    IList<Item> Items;
+    public class GildedRose {
+        private readonly IList<Item> Items;
+        private readonly Dictionary<string, IItemStrategy> Strategies;
 
-    public GildedRose(IList<Item> Items)
-    {
-        this.Items = Items;
-    }
+        public GildedRose(IList<Item> items) {
+            Items = items;
+            Strategies = new Dictionary<string, IItemStrategy> {
+                { "Aged Brie", new AgedBrieStrategy() },
+                { "Backstage passes to a TAFKAL80ETC concert", new BackstagePassesStrategy() },
+                { "Sulfuras, Hand of Ragnaros", new SulfurasStrategy() },
+                { "Default", new DefaultItemStrategy() }
+            };
+        }
 
-    public void UpdateQuality()
-    {
-        for (var i = 0; i < Items.Count; i++)
-        {
-            if (Items[i].Name != "Aged Brie" && Items[i].Name != "Backstage passes to a TAFKAL80ETC concert")
-            {
-                if (Items[i].Quality > 0)
-                {
-                    if (Items[i].Name != "Sulfuras, Hand of Ragnaros")
-                    {
-                        Items[i].Quality = Items[i].Quality - 1;
-                    }
-                }
-            }
-            else
-            {
-                if (Items[i].Quality < 50)
-                {
-                    Items[i].Quality = Items[i].Quality + 1;
-
-                    if (Items[i].Name == "Backstage passes to a TAFKAL80ETC concert")
-                    {
-                        if (Items[i].SellIn < 11)
-                        {
-                            if (Items[i].Quality < 50)
-                            {
-                                Items[i].Quality = Items[i].Quality + 1;
-                            }
-                        }
-
-                        if (Items[i].SellIn < 6)
-                        {
-                            if (Items[i].Quality < 50)
-                            {
-                                Items[i].Quality = Items[i].Quality + 1;
-                            }
-                        }
-                    }
-                }
-            }
-
-            if (Items[i].Name != "Sulfuras, Hand of Ragnaros")
-            {
-                Items[i].SellIn = Items[i].SellIn - 1;
-            }
-
-            if (Items[i].SellIn < 0)
-            {
-                if (Items[i].Name != "Aged Brie")
-                {
-                    if (Items[i].Name != "Backstage passes to a TAFKAL80ETC concert")
-                    {
-                        if (Items[i].Quality > 0)
-                        {
-                            if (Items[i].Name != "Sulfuras, Hand of Ragnaros")
-                            {
-                                Items[i].Quality = Items[i].Quality - 1;
-                            }
-                        }
-                    }
-                    else
-                    {
-                        Items[i].Quality = Items[i].Quality - Items[i].Quality;
-                    }
-                }
-                else
-                {
-                    if (Items[i].Quality < 50)
-                    {
-                        Items[i].Quality = Items[i].Quality + 1;
-                    }
+        public void UpdateQuality() {
+            foreach (var item in Items) {
+                if (Strategies.TryGetValue(item.Name, out var strategy)) {
+                    strategy.Update(item);
+                } else {
+                    Strategies["Default"].Update(item);
                 }
             }
         }
+    }
+
+    public interface IItemStrategy {
+        void Update(Item item);
+    }
+
+    public class AgedBrieStrategy : IItemStrategy {
+        public void Update(Item item) {
+            item.SellIn--;
+            item.Quality = System.Math.Min(50, item.Quality + 1);
+
+            if (item.SellIn < 0) {
+                item.Quality = System.Math.Min(50, item.Quality + 1);
+            }
+        }
+    }
+
+    public class BackstagePassesStrategy : IItemStrategy {
+        public void Update(Item item) {
+            item.SellIn--;
+
+            if (item.SellIn < 0) {
+                item.Quality = 0;
+                return;
+            }
+
+            if (item.SellIn < 5) {
+                item.Quality = System.Math.Min(50, item.Quality + 3);
+            } else if (item.SellIn < 10) {
+                item.Quality = System.Math.Min(50, item.Quality + 2);
+            } else {
+                item.Quality = System.Math.Min(50, item.Quality + 1);
+            }
+        }
+    }
+
+    public class SulfurasStrategy : IItemStrategy {
+        public void Update(Item item) {
+            // "Sulfuras" items do not change in quality or sell-in.
+        }
+    }
+
+    public class DefaultItemStrategy : IItemStrategy {
+        public void Update(Item item) {
+            item.SellIn--;
+            item.Quality = System.Math.Max(0, item.Quality - 1);
+
+            if (item.SellIn < 0) {
+                item.Quality = System.Math.Max(0, item.Quality - 1);
+            }
+        }
+    }
+
+    public class Item {
+        public string Name { get; set; }
+        public int SellIn { get; set; }
+        public int Quality { get; set; }
     }
 }
